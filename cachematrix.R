70891a4b673bccbcade13cf219b8ef28bb0f598c
4ringutan <4ringutan@gmail.com>
2020-04-22 17:11:01 -0400

    Completed programming assignment 2 by creating the functions makecachematrix and cachesolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL ## Initializes object x and m 
+  set <- function(y) {
+    x <<- y ## Sets input x into the parent environment with '<<-'
+    m <<- NULL ## Sets input m into the parent environment with '<<-' and clears any previous values of m
+  }
+  get <- function() x ## Retrieves x from parent environment of makeCacheMatrix
+  setinverse <- function(inverse) m <<- inverse ## Sets inverse of the matrix and m is assigned to parent environment
+  getinverse <- function() m ## Gets inverse of matrix
+  list(set = set, get = get, ## set() and get() are given the names 'set and 'get' respectively so they can be retrieved using '$'
+       setinverse = setinverse, ## setinverse() is given the name 'setinverse' for future retrieval with '$'
+       getinverse = getinverse) ## getinverse() is given the name 'getinverse' for future retrieval with '$'
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse() ##Attempts to retrieve inverse from makeCacheMatrix
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m) ## Checks to see if m is NULL, if it's not then returns the value
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m) ## If m was NULL then it calculates the inverse using solv() and then sets it 
+    m ## Return a matrix that is the inverse of 'x'
 }
